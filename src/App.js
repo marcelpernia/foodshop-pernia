ce93060a205e1fcd1389946cac926a4b7fa52515
Marcel Pernia <marcelpm1987@gmail.com>
2022-08-10 21:49:38 -0500

    Se agrega algo de estructura y estilo para mejorar la presentacion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import './style.css';
+
 function App() {
   return (
-    <div className="App">Mi primera App</div>
+    <div className="app">
+      <div>
+        <h1 className="title">Mi Primera App</h1>
+        <p>Por Marcel Pernía</p>
+        <span>Para Coder House</span>
+      </div>
+    </div>
   );
 }
 
