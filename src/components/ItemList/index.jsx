4d24967d65074a61cc0f59fd8d09fbf7b86058b5
Marcel Pernia <marcelpm1987@gmail.com>
2022-09-03 18:56:24 -0500

    se instala react router. se crea mockup json con data. se refactoriza ItemList para hacer fetch a data local

--- src/components/ItemList/index.jsx
+++ src/components/ItemList/index.jsx
@@ -6,30 +6,13 @@ const ItemList = () => {
     const [products, setProducts] = useState(null)
 
     useEffect(() => {
-        const promesa = new Promise((resolve) => {
-            setTimeout(() =>{
-                resolve([
-                    {
-                        id: 1,
-                        title: 'Organic Lemon',
-                        stock: 20,
-                        description: '',
-                        price: '$1.20 each',
-                        pictureUrl: '/img/lemon.jpg',
-                    },
-                    {
-                        id: 2,
-                        title: 'Gala Apple',
-                        stock: 8,
-                        description: '',
-                        price: '$0.71 each',
-                        pictureUrl: '/img/apple.jpg',
-                    },
-                ])
-            }, 2000)
-        });
+        const getProducts = async () => {
+            const response = await fetch('./data.json')
+            const data = await response.json()
 
-        promesa.then((results) => setProducts(results))
+            setProducts(data)
+        }
+        getProducts()
     }, [])
 
     return (
@@ -40,7 +23,7 @@ const ItemList = () => {
                     title={item.title}
                     stock={item.stock}
                     price={item.price}
-                    image={item.pictureUrl}
+                    image={item.image.small}
                 />
             ))}
         </ItemGrid>
