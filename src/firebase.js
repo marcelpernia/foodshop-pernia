1639617f1a8b7ac06059a7074ed29bed14d72e3b
Marcel Pernia <marcelpm1987@gmail.com>
2022-09-26 23:15:04 -0500

    fix apikey

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCm-6s9BxsK3uMU5EIAQ6sRfAewEtPxZn4",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "foodshop-24d16.firebaseapp.com",
-  projectId: "foodshop-24d16",
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: "foodshop-24d16.appspot.com",
   messagingSenderId: "605235185866",
   appId: "1:605235185866:web:57f3d51c78a8008cd40c33",
 };
 
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 export const db = getFirestore();
-
-// REACT_APP_PROJECT_ID
-// REACT_APP_API_KEY
